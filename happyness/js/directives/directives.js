430610e7aaf2e04af0a9fa8657b46261cd1b1402
jessbringlarsen <jessbringlarsen+github@gmail.com>
2015-01-23 11:58:51 +0100

    Added bar text

--- happyness/js/directives/directives.js
+++ happyness/js/directives/directives.js
@@ -12,36 +12,37 @@ happinessDirectives.directive('graphDirective', function() {
                 {"voteValue": 5, "voteCount": 27}
             ];
 
-            var data = votes.slice();
-
             var voteValueFn = function(d) { return d.voteValue; };
             var voteCountFn = function(d) { return d.voteCount; };
 
             var width = 420,
                 barHeight = 20;
 
-            var x = d3.scale.linear()
-                .domain([0, d3.max(data)])
-                .range([0, width]);
+            // code below assume votes is sorted asc on voteCount
+            var maxVoteCount = voteCountFn(votes[votes.length-1]);
+
+            var barWidth = function(d) {
+                return (voteCountFn(d)/maxVoteCount)*width;
+            };
 
             var chart = d3.select(".chart")
                 .attr("width", width)
-                .attr("height", barHeight * data.length);
+                .attr("height", barHeight * votes.length);
 
             var bar = chart.selectAll("g")
-                .data(data)
+                .data(votes)
                 .enter().append("g")
-                .attr("transform", function(d) { return "translate(0," + voteValueFn(d) * barHeight + ")"; });
+                .attr("transform", function(d) { return "translate(0," + (voteValueFn(d)-1) * barHeight + ")"; });
 
             bar.append("rect")
-                .attr("width", function(d) { return voteCountFn(d) * 10; })
+                .attr("width", function(d) { return barWidth(d); })
                 .attr("height", barHeight - 1);
 
-//            bar.append("text")
-//                .attr("x", function(d) { return x(d) - 3; })
-//                .attr("y", barHeight / 2)
-//                .attr("dy", ".35em")
-//                .text(function(d) { return voteValueFn(d); });
+            bar.append("text")
+                .attr("x", function(d) { return barWidth(d) - 3; })
+                .attr("y", barHeight / 2)
+                .attr("dy", ".35em")
+                .text(function(d) { return voteCountFn(d); });
         }
     };
 });
